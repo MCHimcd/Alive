dd4601a3eeee1a7505774a545703f61f106796d9
Himcd <mc_himcd@163.com>
2024-06-23 19:30:17 +0800

    创建仓库

---
+++ src/main/java/mc/alive/Alive.java
@@ -0,0 +1,17 @@
+package mc.alive;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Alive extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
