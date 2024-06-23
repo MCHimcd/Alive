e1f6f0c255c52daaf9dae0a0a24e8be7cd2ff9b4
Himcd <mc_himcd@163.com>
2024-06-23 19:42:58 +0800

    写了一点

--- src/main/java/mc/alive/Alive.java
+++ src/main/java/mc/alive/Alive.java
@@ -1,17 +1,43 @@
 package mc.alive;
 
+import mc.alive.game.Game;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Alive extends JavaPlugin {
+public final class Alive extends JavaPlugin implements Listener {
+    public static Alive plugin;
+    public static Game game;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        plugin = this;
+        game = new Game();
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        game.destroy();
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        player.setGameMode(GameMode.ADVENTURE);
+        player.clearActivePotionEffects();
+        player.getInventory().clear();
+    }
+
+    @EventHandler
+    public void onHurt(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player) {
+            event.setCancelled(false);
+        }
     }
 }
---
+++ src/main/java/mc/alive/game/Game.java
@@ -0,0 +1,22 @@
+package mc.alive.game;
+
+import static mc.alive.Alive.*;
+
+
+public class Game {
+    public Game() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void end() {
+        game = new Game();
+    }
+
+    public void destroy() {
+
+    }
+}
